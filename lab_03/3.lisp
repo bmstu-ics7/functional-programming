797a0ead741ce953c4c276cff318f1978f1d9306
Willsem <happymainer132@yandex.ru>
2020-02-26 13:22:29 +0300

    :new: add lab_03

---
+++ lab_03/3.lisp
@@ -0,0 +1,5 @@
+(defun volume (a b c)
+    (* a b c)
+)
+
+(volume 2 3 4) ;;; 24
---
+++ lab_03/6.lisp
@@ -0,0 +1,20 @@
+(cons 3 (list 5 6))
+;;; Результат: (3 5 6)
+
+(list 3 'from 9 'lives (- 9 3))
+;;; Результат: (3 FROM 9 LIVES 6)
+
+(+ (length for 2 too) (car '(21 22 23)))
+;;; Результат: ошибка - нет переменной FOR
+
+(cdr ' (cons is short for ans))
+;;; Результат: (IS SHORT FOR ANS)
+
+(car (list one two))
+;;; Результат: ошибка - нет переменной ONE
+
+(cons 3 '(list 5 6))
+;;; Результат: (3 LIST 5 6)
+
+(car (list 'one 'two))
+;;; Результат: (ONE)
