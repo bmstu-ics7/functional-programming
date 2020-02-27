89886cc3267cf129e708a4f4954a894d2b2eaf77
Willsem <happymainer132@yandex.ru>
2020-02-27 20:26:48 +0300

    :pencil: add new method of function

--- lab_03/src/5.lisp
+++ lab_03/src/5.lisp
@@ -2,5 +2,13 @@
     (> (length list1) (length list2))
 )
 
+(defun longer_then (list1 list2)
+    (cond
+        ((null list1) Nil)
+        ((and list1 (null list2)))
+        ((longer_then (cdr list1) (cdr list2)))
+    )
+)
+
 (longer_then '(1 2 3) '(1 2)) ;;; T
 (longer_then '(1 2 3) '(1 2 3 4)) ;;; NIL
